1da4eb77b10f8a7fa8fecb4fdd3e3f954e32c1a0
David Etkin <d.etkin@leasetrack.ai>
2020-12-16 17:19:01 -0500

    move directory

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/www'));
+app.use(express.static(__dirname + '/'));
 
 console.log('--------------------------------------------------------')
 console.log('>>>>>>>>>> Starting app...\n');
@@ -10,4 +10,4 @@ console.log('>>>>>>>>>> Author: David Etkin');
 console.log('>>>>>>>>>> Organization: LeaseTrack');
 console.log('>>>>>>>>>> Date: Dec 2020\n');
 app.listen('8081');
-console.log('>>>>>>>>>> Running at localhost:8081...');
+console.log('>>>>>>>>>> Running at localhost:8081...\n');
