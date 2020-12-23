5d2d68f695c237a71bd80aa6ca95fda63f4946c6
David Etkin <davidetkin@davidetkin.com>
2020-12-23 17:26:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -364,6 +364,37 @@ var participation_pie = new Chart(ctx, {
 });
 
 
+/*
+Chart.plugins.register({
+    afterDatasetsDraw: function(chartInstance, easing) {
+        // To only draw at the end of animation, check for easing === 1
+        var ctx = chartInstance.chart.ctx;
+        chartInstance.data.datasets.forEach(function(dataset, i) {
+            var meta = chartInstance.getDatasetMeta(i);
+            if (!meta.hidden) {
+                meta.data.forEach(function(element, index) {
+                    // Draw the text in black, with the specified font
+                    ctx.fillStyle = 'white';
+                    var fontSize = 8;
+                    var fontStyle = 'bold';
+                    var fontFamily = 'Tahoma';
+                    ctx.font = Chart.helpers.fontString(fontSize, fontStyle, fontFamily);
+                    // Just naively convert to string for now
+                    var dataString = dataset.data[index].toString();
+                    // Make sure alignment settings are correct
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+                    var padding = 0;
+                    var position = element.tooltipPosition();
+                    ctx.fillText(dataString + '%', position.x, position.y);
+                });
+            }
+        });
+    }
+});
+*/
+
+
 $(document).ready(function() {
     $('#dataTable').DataTable();
     // document.getElementById("legend1").innerHTML = participation_pie.generateLegend();
